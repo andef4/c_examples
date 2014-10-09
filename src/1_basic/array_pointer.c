ed88704d8304c47d19dc675360a3c484aba25d9c
Fabio Anderegg <andef4@bfh.ch>
2014-10-09 16:33:49 +0200

    moved sources to 1_basic

--- src/1_basic/array_pointer.c
+++ src/1_basic/array_pointer.c
@@ -12,7 +12,7 @@ typedef struct struct1 struct1_t;
 
 int main(void) {
 	struct struct1 my_struct1;
-	struct1_t my_struct2;
+	//struct1_t my_struct2;
 
 	// the following two lines do not compile:
 	//  struct1 my_struct3;
@@ -21,7 +21,7 @@ int main(void) {
 
 	printf("Expected size: 2 + 4 + 8 = 14, real size: %ld\n", sizeof(my_struct1));
 
-	struct1_t mystruct[5];
+	//struct1_t mystruct[5];
 }
 
 
